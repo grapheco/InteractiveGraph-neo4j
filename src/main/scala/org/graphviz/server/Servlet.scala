130478aa37cab4926342ddc6149697ddb03ae564
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2018-02-12 13:03:37 +0800

    use @Autowired

--- src/main/scala/org/graphviz/server/Servlet.scala
+++ src/main/scala/org/graphviz/server/Servlet.scala
@@ -9,13 +9,14 @@ import org.springframework.web.context.support.WebApplicationContextUtils
 /**
   * Created by bluejoe on 2018/2/5.
   */
+
 class Servlet extends HttpServlet {
   var _beans: WebApplicationContext = null;
   var _commandRegistry: CommandRegistry = null;
 
   override def init(servletConfig: ServletConfig) = {
     _beans = WebApplicationContextUtils.getRequiredWebApplicationContext(servletConfig.getServletContext());
-    _commandRegistry = _beans.getBean("commandRegistry").asInstanceOf[CommandRegistry];
+    _commandRegistry = _beans.getBean(classOf[CommandRegistry]);
   }
 
   override protected def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
@@ -42,7 +43,7 @@ class Servlet extends HttpServlet {
           def contains(name: String): Boolean = req.getParameter(name) != null;
         };
 
-        var ct: ContentTag = new ContentTag {
+        val ct: ContentTag = new ContentTag {
           def setContentType(ct: String) = resp.setContentType(ct);
 
           def setContentLength(len: Int) = resp.setContentLength(len);
--- src/main/scala/org/graphviz/server/commands/Neo4jConnector.scala
+++ src/main/scala/org/graphviz/server/commands/Neo4jConnector.scala
@@ -1,13 +1,85 @@
 package org.graphviz.server.commands
 
 import org.neo4j.driver.v1._
+import org.neo4j.driver.v1.types.Node
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
 
 import scala.collection.JavaConversions._
 import scala.reflect.ClassTag
 
-/**
-  * Created by bluejoe on 2018/2/7.
-  */
+trait GraphMetaDB {
+  def getNodesCount(): Option[Int];
+
+  def getNodeMeta(node: Node): NodeMeta;
+}
+
+trait NodeMeta {
+  def getId(): String;
+
+  def getCaption(): Option[String];
+
+  def getTitle(): Option[String];
+
+  def getSize(): Option[Int];
+
+  def getGroupName(): Option[String];
+
+  def getXY(): Option[(Double, Double)];
+
+  def getColor(): Option[String];
+}
+
+class Neo4jGraphMetaDBInMemory extends GraphMetaDB with InitializingBean {
+  @Autowired
+  var _neo4jConnector: Neo4jConnector = null;
+  var _nodesDegreeMap = collection.mutable.Map[String, Int]();
+
+  var _nodesCount: Option[Int] = None;
+  var _idName: String = "id";
+  var _captionName: Option[String] = None;
+  var _titleName: Option[String] = None;
+
+  def setIdName(value: String) = _idName = value;
+
+  def setCaptionName(value: String) = _captionName = Some(value);
+
+  def setTitleName(value: String) = _titleName = Some(value);
+
+  def updateMeta() = {
+    //count numbers of nodes
+    _nodesCount = Some(_neo4jConnector.querySingleObject("match (n) return count(n)", _.get(0).asInt()));
+    //calculates relations of each node
+    _nodesDegreeMap.clear();
+    _neo4jConnector.queryObjects("MATCH (c)-[]-() WITH c, count(*) AS degree return id(c),degree",
+      (node) => (node.get(0).toString() -> node.get(1).asInt())).foreach(x => _nodesDegreeMap.update(x._1, x._2));
+  }
+
+  @throws(classOf[Exception])
+  def afterPropertiesSet = updateMeta;
+
+  override def getNodesCount(): Option[Int] = _nodesCount;
+
+  override def getNodeMeta(node: Node): NodeMeta = {
+    val map = node.asMap();
+    new NodeMeta() {
+      def getId(): String = map(_idName).toString;
+
+      def getCaption(): Option[String] = _captionName.map(map.get(_).toString);
+
+      def getTitle(): Option[String] = _titleName.map(map.get(_).toString);
+
+      def getSize(): Option[Int] = _nodesDegreeMap.get(getId());
+
+      def getGroupName(): Option[String] = node.labels().headOption;
+
+      def getXY(): Option[(Double, Double)] = None;
+
+      def getColor(): Option[String] = None;
+    };
+  }
+}
+
 class Neo4jConnector {
   var _url = "";
   var _user = "";
