c1e3cba2eebc109b055f68f4d5899d8900fa6978
Airzihao <airzihao@163.com>
2018-05-11 20:16:16 +0800

    add the conf of getNodesCategories (to be corrected)

--- src/main/scala/org/interactivegraph/server/commands/neo4j/Neo4jCommands.scala
+++ src/main/scala/org/interactivegraph/server/commands/neo4j/Neo4jCommands.scala
@@ -68,3 +68,52 @@ class LoadGraph extends JsonOutput with WithNeo4jServer {
     }.toArray
   }
 }
+
+class GetNodesCategories extends JsonOutput with WithNeo4jServer {
+      override def execute(params: Params): Map[String, Any] = {
+        val query = s"match(n) return distinct labels(n)";
+        Map("labels and categories" ->
+        _neo4jConnector.queryObjects(query,{
+          result =>val label = result.fields().get(0).value().get(0).asString();
+            Map[String, Any]("label" -> label);
+        }));
+    }
+}
+
+class GetNeighbours extends JsonOutput with WithNeo4jServer{
+//  override def execute(params: Params): Map[String, Any] = {
+       // val query = s"match (n) where id(n) in [$ids] return n";
+
+//
+//  }
+
+  //val id = params.getStringArray("nodes").reduce(_ + "," + _);
+  val id = 3863;
+  val query_edges = s"start a=node($id) match (a)-[c]-(b) return c";
+  val query_nodes = s"start a=node($id) match (a)-[c]-(b) return b";
+
+  override def execute(params: Params): Map[String, Any] = {
+
+    _neo4jConnector.execute { (session: Session) =>
+      val nodes = queryNodes(session);
+      val edges = queryEdges(session);
+      Map[String, Any]("data" -> Map[String, Any]("nodes" -> nodes, "edges" -> edges));
+    };
+  }
+  private def queryEdges(session: Session): Array[Map[String, Any]] = {
+    session.run(query_edges).map { result =>
+      val rel = result.get("p").asPath().relationships().iterator().next();
+
+      val from = rel.startNodeId();
+      val to = rel.endNodeId();
+      val id = rel.id();
+      val label = rel.`type`();
+      Map[String, Any]("id" -> id, "label" -> label, "from" -> from, "to" -> to);
+    }.toArray
+  }
+
+
+
+
+
+}
