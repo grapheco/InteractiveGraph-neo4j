ec507a9d4b0106554530cd28d9d94d656832fe8e
huchuan <2585814664@qq.com>
2019-04-12 16:36:10 +0800

    Add support for PiDB

---
+++ src/main/scala/org/interactivegraph/server/pidb/services.scala
@@ -0,0 +1,37 @@
+package org.interactivegraph.server.pidb
+
+import java.io.{File, FileInputStream}
+
+import org.apache.commons.io.{FileUtils, IOUtils}
+import org.interactivegraph.server.util.{JsonUtils, Logging, ServletContextUtils}
+import org.neo4j.driver.v1._
+import org.neo4j.graphdb.factory.{GraphDatabaseFactory, GraphDatabaseSettings}
+import org.neo4j.graphdb.{GraphDatabaseService, Label, RelationshipType}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.{DisposableBean, InitializingBean}
+import cn.pidb.engine.{BoltService, CypherService, PidbConnector}
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+/**
+  * Created by huchuan on 2019/4/10.
+  */
+
+class PidbService(boltUrl:String, boltUser:String, boltPassword:String) extends BoltService(boltUrl, boltUser, boltPassword){
+
+
+  def getRelativeOrAbsoluteFile(path: String) = {
+    Some(new File(path)).map { file =>
+      if (file.isAbsolute) {
+        file
+      }
+      else {
+        new File(ServletContextUtils.getServletContext.getRealPath(s"/${path}"))
+      }
+    }.get
+  }
+}
+
+
--- src/main/scala/org/interactivegraph/server/util/VelocityUtils.scala
+++ src/main/scala/org/interactivegraph/server/util/VelocityUtils.scala
@@ -1,13 +1,18 @@
 package org.interactivegraph.server.util
 
-import java.io.{File, StringWriter}
+import java.io.{File, FileOutputStream, StringWriter}
 import java.util.Properties
 
+import cn.pidb.blob.Blob
+import cn.pidb.engine.blob.{BlobIO, InlineBlob, RemoteBlob}
 import org.apache.velocity.app.VelocityEngine
 import org.apache.velocity.tools.ToolManager
 import org.apache.velocity.tools.config.DefaultKey
-
+import org.neo4j.values.storable.{BlobValue, ValueWriter}
+import org.springframework.util.ClassUtils
 import scala.collection.JavaConversions
+import java.io.FileOutputStream
+import java.io.IOException
 
 /**
   * Created by bluejoe on 2018/2/13.
@@ -38,7 +43,25 @@ object VelocityUtils {
       if (expr.startsWith("=")) {
         val expr1 = expr.substring(1);
         ve.evaluate(vc, writer, "", s"#set($$__VAR=$expr1)");
-        vc.get("__VAR");
+        var value = vc.get("__VAR");
+        //if is a blob
+        if(value.isInstanceOf[Blob]){
+          //get blob
+          var result:String = ""
+          try {
+            val data = value.asInstanceOf[Blob].toBytes()
+            val path = ClassUtils.getDefaultClassLoader.getResource("").getPath.replace("/WEB-INF/classes","") + "static/"
+            val tool = new FileSystemTool()
+            result = tool.filesave(data,path, System.currentTimeMillis.toString+".jpg")
+          }
+          catch{
+            case e:Throwable =>
+              print(e.toString)
+          }
+          //TODO url
+          return "http://localhost:9999/graphserver/static/"+result
+        }
+        return value
       }
       else {
         ve.evaluate(vc, writer, "", expr);
@@ -59,4 +82,18 @@ class WrongExpressionException(msg: String, e: Throwable) extends RuntimeExcepti
 @DefaultKey("fileTool")
 class FileSystemTool {
   def exists(path: String) = new File(path).exists();
+
+
+
+  @throws[IOException]
+  def filesave(file: Array[Byte], filePath: String, fileName: String): String = { //目标目录
+    val targetfile = new File(filePath)
+    if (!targetfile.exists) targetfile.mkdirs
+    //二进制流写入
+    val out = new FileOutputStream(filePath + fileName)
+    out.write(file)
+    out.flush()
+    out.close()
+    return  fileName
+  }
 }
